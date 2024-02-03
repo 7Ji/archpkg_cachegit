ddf9f604ff45e56f0c4733fc5f75d539b97fe874
Guoxin Pu <pugokushin@gmail.com>
2024-02-04 00:06:03 +0800

    proper repo creation and opening

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,106 @@
-use std::{env::args, fs::{create_dir, File}, io::Write, path::Path};
-
+use std::path::Path;
 use git2::Repository;
-use pkgbuild::{GitSourceFragment, SourceProtocol};
-
+use pkgbuild::{GitSourceFragment, Source, SourceProtocol};
 use url::Url;
+use clap::Parser;
 
-fn main() {
-    let gmr = args().nth(1).expect("Prefix of git-mirrorer must be declared as the first argument");
-    let pkgbuild = pkgbuild::parse_one(Some("PKGBUILD")).unwrap();
-    println!("If caching failed, make sure you have the following repos configured in your 7Ji/git-mirrorer config:");
-    for source in pkgbuild.sources.iter() {
-        if let SourceProtocol::Git { fragment: _, signed: _ } = source.protocol {
-            print!("  - {}", &source.url);
-            if source.url.ends_with(".git") {
-                println!()
+#[derive(Parser)]
+struct Arg {
+    #[arg(short, long)]
+    /// Fetch all refs, instead of only those declared in sources
+    allrefs: bool,
+    #[arg(short, long)]
+    /// Print a config to be used for 7Ji/git-mirrorer and early quit
+    prconf: bool,
+    /// The prefix of a 7Ji/git-mirrorer instance, e.g. http://gmr.lan
+    gmr: String,
+}
+
+fn fetchspec_from_source(source: &Source, allrefs: bool) -> String {
+    if let SourceProtocol::Git { fragment, signed: _ } = &source.protocol {
+        if allrefs {
+            "*".into()
+        } else {
+            if let Some(fragment) = fragment {
+                match fragment {
+                    GitSourceFragment::Branch(branch) => format!("heads/{}", branch),
+                    GitSourceFragment::Commit(_) => "*".into(),
+                    GitSourceFragment::Tag(tag) => format!("tags/{}", tag),
+                }
             } else {
-                println!(".git")
+                "*".into()
             }
+        }
+    } else {
+        String::new()
+    }
+}
+
+fn open_or_create_repo<P: AsRef<Path>, S: AsRef<str>>(path: P, remote: S) -> Repository {
+    match Repository::open_bare(&path) {
+        Ok(repo) => repo,
+        Err(e) => {
+            eprintln!("Failed to open repo at '{}': {}", path.as_ref().display(), e);
+            match Repository::init_bare(&path) {
+                Ok(repo) => {
+                    println!("Created repo at '{}'", path.as_ref().display());
+                    if let Err(e) = repo.remote_with_fetch("origin", remote.as_ref(), "+refs/*:refs/*") {
+                        eprintln!("Failed to create remote '{}': {}", remote.as_ref(), e);
+                        panic!("Failed to create remote")
+                    }
+                    repo
+                },
+                Err(e) => {
+                    eprintln!("Failed to create repo at '{}': {}", path.as_ref().display(), e);
+                    panic!("Failed to create repo")
+                },
+            }
+        },
+    }
+}
+
+fn cache_source<S: Into<String>>(source: &Source, allrefs: bool, gmr: S) {
+    let fetchspec = fetchspec_from_source(source, allrefs);
+    if fetchspec.is_empty() { return }
+    let repo = open_or_create_repo(&source.name, &source.url);
+    let url = Url::parse(&source.url).expect("Failed to parse git source url");
+    let mut gmr_url = gmr.into();
+    if let Some(domain) = url.domain() {
+        gmr_url.push('/');
+        gmr_url.push_str(domain);
+    }
+    gmr_url.push_str(url.path());
+    let mut remote = repo.remote_anonymous(&gmr_url).expect("Failed to create anonymous remote");
+    println!("Caching git source '{}' from gmr '{}'", source.name, &gmr_url);
+    remote.fetch(&[format!("+refs/{}:refs/{}", fetchspec, fetchspec)], None, None).expect("Failed to fetch from remote");
+    for head in remote.list().expect("Failed to list remote heads") {
+        if head.name() == "HEAD" {
+            if let Some(target) = head.symref_target() {
+                repo.set_head(target).expect("Failed to update local HEAD");
+            }
+            break
         }
     }
-    for source in pkgbuild.sources.iter() {
-        let fetchspec = 
-            if let SourceProtocol::Git { fragment, signed: _ } = &source.protocol {
-                if let Some(fragment) = fragment {
-                    match fragment {
-                        GitSourceFragment::Branch(branch) => format!("heads/{}", branch),
-                        GitSourceFragment::Commit(_) => "*".into(),
-                        GitSourceFragment::Tag(tag) => format!("tags/{}", tag),
-                    }
+}
+
+fn main() {
+    let arg = Arg::parse();
+    let pkgbuild = pkgbuild::parse_one(Some("PKGBUILD")).unwrap();
+    if arg.prconf {
+        println!("repos:");
+        for source in pkgbuild.sources.iter() {
+            if let SourceProtocol::Git { fragment: _, signed: _ } = source.protocol {
+                print!("  - {}", &source.url);
+                if source.url.ends_with(".git") {
+                    println!()
                 } else {
-                    "*".into()
+                    println!(".git")
                 }
-            } else {
-                continue
-            };
-        let repo = Path::new(&source.name);
-        if repo.exists() {
-            if ! repo.is_dir() {
-                eprintln!("{} exists and is not a dir", &source.name);
-                panic!("Path of git repo occupied by non-dir file");
-            }
-        } else {
-            create_dir(repo).expect("Failed to create git repo dir");
-            for suffix in ["objects", "objects/pack", "refs"] {
-                create_dir(repo.join(suffix)).expect("Failed to create git repo subdir");
-            }
-            let mut head = File::create(repo.join("HEAD")).expect("Failed to create HEAD");
-            head.write_all("ref: refs/heads/master\n".as_bytes()).expect("Failed to write to HEAD");
-            let mut config = File::create(repo.join("config")).expect("Failed to create config");
-            write!(config, "\
-                [core]\n\
-                \trepositoryformatversion = 0\n\
-                \tfilemode = true\n\
-                \tbare = true\n\
-                [remote \"origin\"]\n\
-                \turl = {}\n\
-                \tfetch = +refs/*:refs/*\n",
-                source.url
-            ).expect("Failed to write to git config");
-        }
-        let repo = Repository::open_bare(repo).expect("Failed to open git repo");
-        let url = Url::parse(&source.url).expect("Failed to parse git source url");
-        let mut gmr_url = gmr.clone();
-        if let Some(domain) = url.domain() {
-            gmr_url.push('/');
-            gmr_url.push_str(domain);
-        }
-        gmr_url.push_str(url.path());
-        let mut remote = repo.remote_anonymous(&gmr_url).expect("Failed to create anonymous remote");
-        println!("Caching git source '{}' from gmr '{}'", source.name, &gmr_url);
-        remote.fetch(&[format!("+refs/{}:refs/{}", fetchspec, fetchspec)], None, None).expect("Failed to fetch from remote");
-        for head in remote.list().expect("Failed to list remote heads") {
-            if head.name() == "HEAD" {
-                if let Some(target) = head.symref_target() {
-                    repo.set_head(target).expect("Failed to update local HEAD");
-                }
-                break
             }
         }
+        return
+    }
+    for source in pkgbuild.sources.iter() {
+        cache_source(source, arg.allrefs, &arg.gmr);
     }
 }
